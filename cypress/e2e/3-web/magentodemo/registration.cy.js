07a69cca226710373d6afcac5d7674cf7b34b536
Fifimrlsr <rafidahmarlasari@gmail.com>
2024-02-05 20:23:44 +0700

    6 test cases of registration

--- cypress/e2e/3-web/magentodemo/registration.cy.js
+++ cypress/e2e/3-web/magentodemo/registration.cy.js
@@ -1,5 +1,5 @@
 describe('Registration', () => {
-  it('Create Account', () => {
+  it('Success Create Account', () => {
     cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
     cy.get('#firstname').type('rafidah')
     cy.get('#lastname').type('marlasari')
@@ -9,4 +9,58 @@ describe('Registration', () => {
     cy.get('#form-validate').click()
     cy.url().should('contain','https://magento.softwaretestingboard.com/customer/account/')
   })
-})
+
+  //it('Failed Create Account - Empty First Name', () => {
+    //cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
+    //cy.get('#firstname').type('rafidah')
+    //cy.get('#lastname').type('marlasari')
+    //cy.get('#email_address').type('rafidahmarlasari@gmail.com')
+    //cy.get('#password').type('Password123')
+    //cy.get('#password-confirmation').type('Password123')
+    //cy.get('#form-validate').click()
+    //cy.url().should('contain','https://magento.softwaretestingboard.com/customer/account/')
+  })
+
+  it('Failed Create Account - Invalid email', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
+    cy.get('#firstname').type('rafidah')
+    cy.get('#lastname').type('marlasari')
+    cy.get('#email_address').type('rafidahmarlasarigmail.com')
+    cy.get('#password').type('Password123')
+    cy.get('#password-confirmation').type('Password123')
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action > span').click()
+    cy.get('#email_address-error').should('contain.text','Please enter a valid email address (Ex: johndoe@domain.com).')
+  })
+
+  it('Failed Create Account - invalid password (<8 symbols)', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
+    cy.get('#firstname').type('rafidah')
+    cy.get('#lastname').type('marlasari')
+    cy.get('#email_address').type('rafidahmarlasari@gmail.com')
+    cy.get('#password').type('123')
+    cy.get('#password-error').should('contain.text','Minimum length of this field must be equal or greater than 8 symbols. Leading and trailing spaces will be ignored.')
+    cy.get('#password-confirmation').type('123')
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action > span').click()
+  })
+
+  it('Failed Create Account - invalid password (>8 symbols)', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
+    cy.get('#firstname').type('rafidah')
+    cy.get('#lastname').type('marlasari')
+    cy.get('#email_address').type('rafidahmarlasari@gmail.com')
+    cy.get('#password').type('123456789')
+    cy.get('#password-error').should('contain.text','Minimum of different classes of characters in password is 3. Classes of characters: Lower Case, Upper Case, Digits, Special Characters.')
+    cy.get('#password-confirmation').type('123456789')
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action > span').click()
+  })
+
+  it.only('Failed Create Account - Wrong confirm password', () => {
+    cy.visit('https://magento.softwaretestingboard.com/customer/account/create/')
+    cy.get('#firstname').type('rafidah')
+    cy.get('#lastname').type('marlasari')
+    cy.get('#email_address').type('rafidahmarlasarigmail.com')
+    cy.get('#password').type('Password123')
+    cy.get('#password-confirmation').type('Pass123')
+    cy.get('#form-validate > .actions-toolbar > div.primary > .action > span').click()
+    cy.get('#password-confirmation-error').should('contain.text','Please enter the same value again.')
+  })
