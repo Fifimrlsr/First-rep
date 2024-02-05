b50ab0ba651f5ba040d0fcfcf9d6fe1b6665c7d0
Fifimrlsr <rafidahmarlasari@gmail.com>
2024-02-05 09:45:23 +0700

    2nd test case for login

--- cypress/e2e/3-web/magentodemo/login.cy.js
+++ cypress/e2e/3-web/magentodemo/login.cy.js
@@ -1,8 +1,18 @@
 describe('Login Functionality', () => {
-  it('Verify Failed Login', () => {
-    cy.visit('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/')
+    beforeEach(() => {
+      cy.visit('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/')
+    })
+  it('Verify Failed Login - Wrong Creds', () => {
     cy.get('#email').type('fifi@gmail.com')
     cy.get('#pass').type('1234')
     cy.get('#send2').click()
+    cy.get('.message-error > div').should('be.visible')
+  })
+  it('Verify Failed Login - Wrong Email', () => {
+    cy.get('#email').type("aaaa")
+    cy.get('#pass').type("1234")
+    cy.get('#send2').click()
+    cy.get('#email-error').should('be.visible')
   })
 })
+
